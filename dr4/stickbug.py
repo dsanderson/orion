8c90db54316a77c859d40d81b38a6feada797a87
David Anderson <the.real.david.anderson@gmail.com>
2015-09-23 21:52:26 +0800

    Updating

---
+++ dr4/stickbug.py
@@ -0,0 +1,6 @@
+import collections
+import numpy as np
+import matplotlib.pyplot as plt
+
+Motor = collections.namedtuple('Motor',['direction'])
+Link = collections.namedtuple('Link',['direction','length'])
